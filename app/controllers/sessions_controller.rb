25e3e95cf755a33527f058a6849f8d9f00988bfb
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-11-18 05:03:33 +0900

    Implement logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,9 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
     redirect_to root_url, notice: 'ログインしました'
   end
+
+  def destroy
+    reset_session
+    redirect_to root_url, notice: 'ログアウトしました'
+  end
 end
