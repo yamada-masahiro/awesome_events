ba96fc2d86d7b9e37330491cf51df210d0c16326
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-11-19 14:49:55 +0900

    AE-6 Implement attendance of event

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,19 @@
 class TicketsController < ApplicationController
+  def new
+    # events#show にて logged_in? == false の場合、
+    # 「参加する」リンクをクリックすると、このアクションが呼び出される
+    raise ActionController::RoutingError, '未ログイン状態で TicketsController#new にアクセス'
+  end
+
+  def create
+    event = Event.find params[:event_id]
+    @ticket = current_user.tickets.build do |t|
+      t.event_id = event.id
+      t.comment = params[:ticket][:comment]
+    end
+
+    if @ticket.save
+      redirect_to event, notice: 'このイベントに参加表明しました'
+    end
+  end
 end
