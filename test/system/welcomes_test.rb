38a87de43f41959343ff4dac864428290faecbfb
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-11-22 08:06:01 +0900

    AE-9 Add test to show only future events

--- test/system/welcomes_test.rb
+++ test/system/welcomes_test.rb
@@ -6,4 +6,16 @@ class WelcomesTest < ApplicationSystemTestCase
   
     assert_selector "h1", text: "イベント一覧"
   end
+
+  test "show future event but no past one on the root page" do
+    now = Time.zone.now
+    future_event = FactoryBot.create(:event, start_at: now + 3.days)
+    past_event   = FactoryBot.create(:event, start_at: now + 1.day )
+
+    travel_to now + 2.days do
+      visit root_url
+      assert_selector    'h5', text: future_event.name
+      assert_no_selector 'h5', text:   past_event.name
+    end
+  end
 end
