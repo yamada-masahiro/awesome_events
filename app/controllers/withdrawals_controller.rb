a6cbccab3da1f13d7e69a9892ad8ee9c75c262e2
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-11-21 14:04:49 +0900

    AE-7 Implement withdrawal

--- app/controllers/withdrawals_controller.rb
+++ app/controllers/withdrawals_controller.rb
@@ -1,2 +1,11 @@
 class WithdrawalsController < ApplicationController
+  def new
+  end
+
+  def create
+    if current_user.destroy
+      reset_session
+      redirect_to root_url, notice: '退会しました'
+    end
+  end
 end
