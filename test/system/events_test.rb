a7c40e9a987727c2199b51732ede2089c081570d
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-11-21 18:22:55 +0900

    AE-9 Write `system/events_test.rb`

--- test/system/events_test.rb
+++ test/system/events_test.rb
@@ -1,9 +1,10 @@
 require "application_system_test_case"
 
 class EventsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit events_url
-  #
-  #   assert_selector "h1", text: "Event"
-  # end
+  test "show /events/:id" do
+    event = FactoryBot.create(:event)
+    visit event_url(event)
+  
+    assert_selector "h1", text: event.name
+  end
 end
