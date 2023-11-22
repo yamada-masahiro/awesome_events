f32bd463b241ec2e21d1d99bac5d2ba523784eba
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-11-22 17:48:27 +0900

    AE-11 Write `Event` model test

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#created_by? true" do
+    event = FactoryBot.create :event
+    assert event.created_by? event.owner
+  end
+
+  test "#created_by? false" do
+    event = FactoryBot.create :event
+    another_user = FactoryBot.create :event
+    assert_not event.created_by? another_user
+  end
+
+  test "#created_by? arg is nil" do
+    event = FactoryBot.create :event
+    assert_not event.created_by? nil
+  end
 end
