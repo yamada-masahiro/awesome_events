79a2032825887e9d5b7c6c473d0860d4bd942f67
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-11-22 20:52:29 +0900

    AE-11 Add validation tests and correct mock
    The test using `Minitest::mock` does work.
    
    wrong:   `MiniTest`
    correct: `Minitest`

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -27,8 +27,24 @@ class EventTest < ActiveSupport::TestCase
 
   test "#created_by? true using mock" do
     event = FactoryBot.create :event
-    user = MiniTest::Mock.new.expect(:id, event.owner_id)
+    user = Minitest::Mock.new.expect(:id, event.owner_id)
     assert event.created_by? user
     user.verify
+  end
+
+  test "'start_at should be before end_at' validation pass" do
+    start_at = rand(1 ..30).days.from_now
+    end_at = start_at + rand(1 ..30).hours
+    event = FactoryBot.build :event, start_at: start_at, end_at: end_at
+    assert event.valid?
+    assert_empty event.errors[:start_at]
+  end
+
+  test "'start_at should be before end_at' validation doesn't pass" do
+    start_at = rand(1 ..30).days.from_now
+    end_at = start_at - rand(1 ..30).hours
+    event = FactoryBot.build :event, start_at: start_at, end_at: end_at
+    assert_not event.valid?
+    assert_not_empty event.errors[:start_at]
   end
 end
