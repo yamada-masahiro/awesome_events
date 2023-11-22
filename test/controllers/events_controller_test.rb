c4fc3d47b3c316bd7edeea872f9deee725d9d74d
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-11-22 17:21:18 +0900

    AE-10 Add `Event Controller` test

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user can delete event he created" do
+    event_owner = FactoryBot.create :user
+    event = FactoryBot.create :event, owner: event_owner
+    sign_in_as event_owner
+    assert_difference 'Event.count', -1 do
+      delete event_path(event)
+    end
+  end
 end
