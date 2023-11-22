5317fe5566e0cf680dabaf51b5c27e2c90757073
YAMADA Masahiro <yamada_masahiro@nifty.com>
2023-11-22 17:33:26 +0900

    AE-10 Add another `Event Controller` test

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -9,4 +9,17 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
       delete event_path(event)
     end
   end
+
+  test "no one can delete event another user created" do
+    sign_in_user = FactoryBot.create :user
+    event_owner  = FactoryBot.create :user
+    event = FactoryBot.create :event, owner: event_owner
+
+    sign_in_as sign_in_user
+    assert_no_difference 'Event.count' do
+      assert_raises ActiveRecord::RecordNotFound do
+        delete event_path(event)
+      end
+    end
+  end
 end
